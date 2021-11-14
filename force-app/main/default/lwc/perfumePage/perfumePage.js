a7f12d81e317d7bee755222332d6dc2c926dc334
Paweł Sadowski <pawel.sadowski@britenet.com.pl>
2021-11-14 12:33:13 +0100

    working reviews, perfume score not refreshing

--- force-app/main/default/lwc/perfumePage/perfumePage.js
+++ force-app/main/default/lwc/perfumePage/perfumePage.js
@@ -1,15 +1,22 @@
 import { LightningElement,api,track } from 'lwc';
 import getDetailPerfume from '@salesforce/apex/PerfumesController.getDetailPerfume';
+import addReview from '@salesforce/apex/PerfumesController.addReview';
+import getReviews from '@salesforce/apex/PerfumesController.getReviews';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 
 export default class PerfumePage extends LightningElement {
     id;
     @api perfume;
+    @track reviews = [];
+    reviesExists;
     topNotes = []
     middleNotes =[]
     baseNotes = []
     mainAccords;
     option = "";
-    score = '1';
+    @track score = '1';
+    @track reviewText = "";
 
     connectedCallback(){
         let incomeURL = window.location.href;
@@ -24,6 +31,7 @@ export default class PerfumePage extends LightningElement {
             .then(result => {
                 this.perfume = result;
                 this.setNotes(this.perfume);
+                this.getAllReviews();
             })
             .catch(error => {
                 this.error = error;
@@ -35,7 +43,18 @@ export default class PerfumePage extends LightningElement {
         this.baseNotes = perfume.baseNotes.split(';')
         this.mainAccords = perfume.mainAccords.split(';')
     }
-
+    getAllReviews(){
+        console.log('wchodzi')
+        getReviews({id:this.id})
+            .then(result => {
+                this.reviews = result;
+                this.reviesExists = result.length > 0;
+                console.log(JSON.stringify(result))
+            })
+            .catch(error => {
+                this.error = error;
+            });   
+    }
 
     get options() {
         var returnOptions = [];
@@ -54,5 +73,53 @@ export default class PerfumePage extends LightningElement {
             { label: '4', value: '4' },
             { label: '5', value: '5' },
         ];
+    }
+
+    scoreChange(event) {
+        this.score= event.target.value;
+    }
+
+    reviewChange(event) {
+        this.reviewText= event.target.value;
+    }
+
+    addReview(){
+        console.log(this.reviewText)
+        console.log(this.score)
+        if(this.reviewText != ""){
+            addReview({text:this.reviewText,score:this.score,perfume:this.id})
+            .then(result => {
+                if(result == true){
+                    const evt = new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Review added succesfully',
+                        variant: 'succes',
+                        mode: 'dismissable'
+                    });
+                    this.dispatchEvent(evt);
+                    this.getDetails(this.id);
+                    this.getAllReviews();
+                }else{
+                    const evt = new ShowToastEvent({
+                        title: 'Error',
+                        message: 'Cannot add review',
+                        variant: 'error',
+                        mode: 'dismissable'
+                    });
+                    this.dispatchEvent(evt);
+                }
+            })
+            .catch(error => {
+                this.error = error;
+            }); 
+        }else{
+            const evt = new ShowToastEvent({
+                title: 'Wrong Review',
+                message: 'Review cannot be empty',
+                variant: 'warning',
+                mode: 'dismissable'
+            });
+            this.dispatchEvent(evt);
+        }
     }
 }
